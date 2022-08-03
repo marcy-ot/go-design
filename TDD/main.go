2b4788986b500a7a8655d7b31fa317a93826f944
大谷将司 <masashi.fudin@gmail.com>
2022-08-03 22:47:09 +0900

    TDDによる実装

---
+++ TDD/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	csv := NewCsvUtil("./testdata/game_score.csv")
+	data := csv.Parse()
+
+	scoreBoad := NewScoreBoad(data)
+	scoreBoad.sumScoreByPlayer()
+	scoreBoad.addRank()
+	result := scoreBoad.cut(10)
+
+	result.printConsole()
+}
+
+type CsvUtil struct {
+	Path    string
+	RawData [][]string
+}
+
+func NewCsvUtil(path string) *CsvUtil {
+	return &CsvUtil{
+		Path: path,
+	}
+}
+
+func (c CsvUtil) Parse() [][]string {
+	file, err := os.Open(c.Path)
+	if err != nil {
+		log.Fatalf("faild file when open %v", err)
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	rawData, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("faild file when reading %v", err)
+	}
+	// TODO: 正常値がチェック
+
+	return rawData
+}
+
+type Score struct {
+	playerID int
+	score    int
+	rank     int
+}
+
+type ScoreBoad struct {
+	scores []Score
+}
+
+var fileHeader = []string{
+	"create_timestamp",
+	"player_id",
+	"score",
+}
+
+var consoleHeader = []string{
+	"rank",
+	"score",
+	"player_id",
+}
+
+func NewScoreBoad(scoreList [][]string) *ScoreBoad {
+	var scoresList []Score
+	for _, v := range scoreList {
+		if reflect.DeepEqual(fileHeader, v) {
+			continue
+		}
+		playerID, _ := strconv.Atoi(v[1])
+		score, _ := strconv.Atoi(v[2])
+		scoreList := Score{
+			playerID: playerID,
+			score:    score,
+		}
+		scoresList = append(scoresList, scoreList)
+	}
+
+	return &ScoreBoad{
+		scores: scoresList,
+	}
+}
+
+func (s ScoreBoad) sumScoreByPlayer() *ScoreBoad {
+	aggScoreList := NewScoreBoad([][]string{})
+	for _, data := range s.scores {
+		if aggScoreList.hasPlayer(data.playerID) {
+			aggScoreList.addScore(data.playerID, data.score)
+		} else {
+			aggScoreList.appendScore(data.playerID, data.score)
+		}
+	}
+	return aggScoreList
+}
+
+func (s ScoreBoad) hasPlayer(id int) bool {
+	for _, v := range s.scores {
+		if v.playerID == id {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *ScoreBoad) addScore(id, score int) {
+	for i, v := range s.scores {
+		if v.playerID == id {
+			s.scores[i].score += score
+		}
+	}
+}
+
+func (s *ScoreBoad) appendScore(id, score int) {
+	scoreList := Score{
+		playerID: id,
+		score:    score,
+	}
+	s.scores = append(s.scores, scoreList)
+}
+
+func (s *ScoreBoad) addRank() {
+	// スコアが高い順にソート
+	sort.SliceStable(s.scores, func(i, j int) bool {
+		return s.scores[i].score > s.scores[j].score
+	})
+
+	rank := 1
+	for i, v := range s.scores {
+		if i == 0 {
+			s.scores[i].rank = rank
+			continue
+		}
+		if s.scores[i-1].score > v.score {
+			rank = i + 1
+		}
+		s.scores[i].rank = rank
+	}
+}
+
+func (s ScoreBoad) cut(limit int) *ScoreBoad {
+	result := NewScoreBoad([][]string{})
+	for i, v := range s.scores {
+		if i != 0 {
+			// 直前のランクと同じランクかどうか
+			samePreviousRank := v.rank == s.scores[i-1].rank
+			if (i >= limit) && !samePreviousRank {
+				break
+			}
+		}
+		result.scores = append(result.scores, v)
+	}
+	return result
+}
+
+func (s ScoreBoad) printConsole() {
+	fmt.Println("rank,player_id,mean_score")
+	for _, v := range s.scores {
+		fmt.Printf("%v,%v,%v\n", v.rank, v.playerID, v.score)
+	}
+}
---
+++ TDD/main_test.go
@@ -0,0 +1,460 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestZeroCsvParse(t *testing.T) {
+	tests := []struct {
+		name string
+		want [][]string
+		path string
+	}{
+		{
+			name: "０件データのCSVファイル読み込み",
+			path: "./testdata/game_score_no_data.csv",
+		}, {
+			name: "正常なCSVファイル読み込み",
+			want: [][]string{
+				{
+					"create_timestamp",
+					"player_id",
+					"score",
+				},
+				{
+					"2021/10/1",
+					"1",
+					"0",
+				}, {
+					"2021/10/1",
+					"2",
+					"123",
+				},
+			},
+			path: "./testdata/game_score_nomal.csv",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewCsvUtil(tt.path)
+			got := c.Parse()
+			if !reflect.DeepEqual(tt.want, got) {
+				fmt.Printf("len %v, cap %v, val %v\n", len(got), cap(got), got)
+				fmt.Printf("len %v, cap %v, val %v\n", len(tt.want), cap(tt.want), tt.want)
+				t.Errorf("data = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewScoreBoad(t *testing.T) {
+	scoreBoad := NewScoreBoad([][]string{
+		{
+			"create_timestamp",
+			"player_id",
+			"score",
+		},
+		{
+			"2021/10/1",
+			"1",
+			"0",
+		}, {
+			"2021/10/1",
+			"2",
+			"123",
+		},
+	})
+	want := &ScoreBoad{
+		scores: []Score{
+			{
+				playerID: 1,
+				score:    0,
+				rank:     0,
+			},
+			{
+				playerID: 2,
+				score:    123,
+				rank:     0,
+			},
+		},
+	}
+	t.Run("正常系のデータをScoreにマッピングする", func(t *testing.T) {
+		if !reflect.DeepEqual(scoreBoad, want) {
+			t.Errorf("scoreBoad = %v, want %v", scoreBoad, want)
+		}
+	})
+
+	scoreBoad = NewScoreBoad([][]string{})
+	want = &ScoreBoad{}
+	t.Run("空リストのマッピング", func(t *testing.T) {
+		if !reflect.DeepEqual(scoreBoad, want) {
+			t.Errorf("scoreBoad = %v, want %v", scoreBoad, want)
+		}
+	})
+}
+
+func TestSumScore(t *testing.T) {
+	want := NewScoreBoad([][]string{
+		{
+			"2021/10/1",
+			"1",
+			"0",
+		}, {
+			"2021/10/1",
+			"2",
+			"300",
+		},
+	})
+	scoreBoad := NewScoreBoad([][]string{
+		{
+			"create_timestamp",
+			"player_id",
+			"score",
+		},
+		{
+			"2021/10/1",
+			"1",
+			"0",
+		}, {
+			"2021/10/1",
+			"2",
+			"123",
+		}, {
+			"2021/10/1",
+			"2",
+			"177",
+		},
+	})
+	result := scoreBoad.sumScoreByPlayer()
+	t.Run("ユーザースコアの加算", func(t *testing.T) {
+		if !reflect.DeepEqual(want, result) {
+			t.Errorf("result = %v, want %v", result, want)
+		}
+	})
+}
+
+func TestAddRank(t *testing.T) {
+	want := &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 4,
+				score:    100,
+				rank:     2,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    70,
+				rank:     4,
+			},
+			Score{
+				playerID: 1,
+				score:    50,
+				rank:     5,
+			},
+			Score{
+				playerID: 6,
+				score:    40,
+				rank:     6,
+			},
+			Score{
+				playerID: 7,
+				score:    30,
+				rank:     7,
+			},
+		},
+	}
+	scoreBoad := NewScoreBoad([][]string{
+		{
+			"create_timestamp",
+			"player_id",
+			"score",
+		},
+		{
+			"2021/10/1",
+			"1",
+			"50",
+		}, {
+			"2021/10/1",
+			"2",
+			"70",
+		}, {
+			"2021/10/1",
+			"3",
+			"90",
+		}, {
+			"2021/10/1",
+			"4",
+			"100",
+		}, {
+			"2021/10/1",
+			"5",
+			"200",
+		}, {
+			"2021/10/1",
+			"6",
+			"40",
+		}, {
+			"2021/10/1",
+			"7",
+			"30",
+		},
+	})
+	scoreBoad.addRank()
+	t.Run("ランキング付与（スコアの重複がない場合）", func(t *testing.T) {
+		if !reflect.DeepEqual(scoreBoad, want) {
+			t.Errorf("got = %v, want %v", scoreBoad, want)
+		}
+	})
+
+	want = &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 4,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 1,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 6,
+				score:    40,
+				rank:     6,
+			},
+			Score{
+				playerID: 7,
+				score:    30,
+				rank:     7,
+			},
+		},
+	}
+	scoreBoad = NewScoreBoad([][]string{
+		{
+			"create_timestamp",
+			"player_id",
+			"score",
+		},
+		{
+			"2021/10/1",
+			"1",
+			"90",
+		}, {
+			"2021/10/1",
+			"2",
+			"90",
+		}, {
+			"2021/10/1",
+			"3",
+			"90",
+		}, {
+			"2021/10/1",
+			"4",
+			"200",
+		}, {
+			"2021/10/1",
+			"5",
+			"200",
+		}, {
+			"2021/10/1",
+			"6",
+			"40",
+		}, {
+			"2021/10/1",
+			"7",
+			"30",
+		},
+	})
+	scoreBoad.addRank()
+	t.Run("ランキング付与（スコアの重複があり場合）", func(t *testing.T) {
+		if !reflect.DeepEqual(scoreBoad, want) {
+			t.Errorf("\ngot = %v\nwant %v", scoreBoad, want)
+		}
+	})
+}
+
+func TestRecordLimit(t *testing.T) {
+	want := &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 4,
+				score:    100,
+				rank:     2,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    70,
+				rank:     4,
+			},
+			Score{
+				playerID: 1,
+				score:    50,
+				rank:     5,
+			},
+		},
+	}
+	scoreBoad := &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 4,
+				score:    100,
+				rank:     2,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    70,
+				rank:     4,
+			},
+			Score{
+				playerID: 1,
+				score:    50,
+				rank:     5,
+			},
+			Score{
+				playerID: 6,
+				score:    40,
+				rank:     6,
+			},
+			Score{
+				playerID: 7,
+				score:    30,
+				rank:     7,
+			},
+		},
+	}
+	result := scoreBoad.cut(5)
+	t.Run("上位5名のレコードに絞る(ランク重複なし）", func(t *testing.T) {
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("scoreBoad = %v, want %v", result, want)
+		}
+	})
+
+	want = &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 4,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 1,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 6,
+				score:    90,
+				rank:     3,
+			},
+		},
+	}
+	scoreBoad = &ScoreBoad{
+		scores: []Score{
+			Score{
+				playerID: 4,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 5,
+				score:    200,
+				rank:     1,
+			},
+			Score{
+				playerID: 1,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 2,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 3,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 6,
+				score:    90,
+				rank:     3,
+			},
+			Score{
+				playerID: 7,
+				score:    30,
+				rank:     7,
+			},
+		},
+	}
+	result = scoreBoad.cut(5)
+	t.Run("上位5名のレコードに絞る(ランク重複あり）", func(t *testing.T) {
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("scoreBoad = %v, want %v", result, want)
+		}
+	})
+}
